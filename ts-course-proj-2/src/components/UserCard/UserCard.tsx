e8ef84d09f3d28486ff170cb98c4bbe5525377e4
Vladimir <djbobah@mail.ru>
2024-03-21 15:50:44 +0300

    10.8 done

--- ts-course-proj-2/src/components/UserCard/UserCard.tsx
+++ ts-course-proj-2/src/components/UserCard/UserCard.tsx
@@ -1,11 +1,17 @@
 import { LocalGitHubUser } from "../../types";
 import { UserStat } from "../UserStat";
+import { UserTitle } from "../UserTitle";
 import styles from "./UserCard.module.scss";
 
 interface UserCardProps extends LocalGitHubUser {}
 
 export const UserCard = (props: UserCardProps) => (
   <div className={styles.userCard}>
+    <img src={props.avatar} alt={props.login} className={styles.avatar} />
+    <UserTitle created={props.created} login={props.login} name={props.name} />
+    <p className={`${styles.bio}${props.bio ? "" : ` ${styles.empty}`} `}>
+      {props.bio || "This profile has no bio"}
+    </p>
     <UserStat
       repos={props.repos}
       followers={props.followers}
