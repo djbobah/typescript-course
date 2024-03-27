3a0a653426768a31c80cf39d216d09069c193726
Vladimir Kirpichev <djbobah@mail.ru>
2024-03-27 20:52:28 +0300

    course finish

--- ts-course-redux-proj/src/features/details/details-slice.ts
+++ ts-course-redux-proj/src/features/details/details-slice.ts
@@ -1,5 +1,4 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { RootState } from "store";
 import { Country, Extra, Status } from "types";
 
 export const loadCountryByName = createAsyncThunk<
